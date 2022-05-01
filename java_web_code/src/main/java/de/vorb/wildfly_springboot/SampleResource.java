c9bcf1dff058f4b380004d960bebe6bb258a6f27
krishita-7 <68993748+krishita-7@users.noreply.github.com>
2022-05-01 14:38:00 +0530

    Update SampleResource.java

--- java_web_code/src/main/java/de/vorb/wildfly_springboot/SampleResource.java
+++ java_web_code/src/main/java/de/vorb/wildfly_springboot/SampleResource.java
@@ -2,13 +2,25 @@ package de.vorb.wildfly_springboot;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
+import java.io.*; 
 @RestController
 public class SampleResource {
 
     @RequestMapping("/")
-    public String hello() {
-        return "Hello World!";
+    
+
+
+// Driver code 
+    public static void main(String arg[]) {
+
+// Change here to change output
+
+        int n = 5;
+
+        for (int i = 1; i <= 10; ++i) 
+            System.out.println(n + "*"+ i +"="+ n * i);
+
     }
 
 }
+
